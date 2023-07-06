e314e02108b0f1775eb69d01aab96d55327bb164
samydoesit <sxburk@gmail.com>
2023-07-06 18:53:39 +0200

    initial commit

---
+++ apps/nuxt/app.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Button from '@ui/components/button/Button.vue'
+</script>
+
+<template>
+  <h1>nuxt monorepo import repro</h1>
+  <Button prefix="test" type="primary">
+    Button1
+  </Button>
+</template>
